ad0eff4d110532299e63e0dc1cc992c7de72e0c4
necharhuso <husnuberkekaya@gmail.com>
2024-03-05 08:37:47 +0300

    s

--- src/uwb_subscriber_node.cpp
+++ src/uwb_subscriber_node.cpp
@@ -90,7 +90,7 @@ private:
     {
         double robotRotationZ = robot_z;
         double robotRotationW = robot_w;
-        double robotRotationYaw = (atan2(2.0*(robotRotationW*robotRotationZ), 1.0 - 2.0*(robotRotationZ*robotRotationZ)))*180.0/M_PI ;
+        double robotRotationYaw = (atan2(2.0*(robotRotationW*robotRotationZ), 1.0 - 2.0*(robotRotationZ*robotRotationZ)))*180.0/M_PI + 90;
         double robotYaw;
         if(robotRotationYaw >180)
         {
@@ -105,33 +105,37 @@ private:
         return robotYaw;
     }
  
-    void rotate_coordinates(double robotPositionX, double robotPositionY, double polygon_latitude, double polygon_longitude, double robotRotationYaw)
+   
+    void rotateCoordinates(double robotPositionX, double robotPositionY, double robotRotationYaw) 
     {
-        
-        // resion of yhe 0.5  in front of the robot polygon start point 
-        // this calculation based for rotated 90 degrees referance coordinates system (a.k.a map frame)
-        arma::vec estimatedRobot_y_coordinates = {polygon_longitude};
-        arma::vec estimatedRobot_x_coordinates = {polygon_latitude};
-
-        arma::mat points(2, estimatedRobot_x_coordinates.size());
-        points.row(0) = estimatedRobot_x_coordinates.t(); 
-        points.row(1) = estimatedRobot_y_coordinates.t();
-
-        double radians = arma::datum::pi / 180.0 * robotRotationYaw;
-        arma::mat22 rotationMatrix;
-        rotationMatrix << std::cos(radians) << std::sin(radians) << arma::endr
-                       << -std::sin(radians) << std::cos(radians) << arma::endr;
-
-        arma::mat rotatedPoints = rotationMatrix * points;
-
-        // Update the global coordinates of the polygon
-        // Assuming you want the first element of the resulting operation
-        objecPositionX_ = (rotatedPoints.row(1) + robotPositionX).eval()(0,0);
-        objecPositionY_ = (rotatedPoints.row(0) + robotPositionY).eval()(0,0);
-
-        printf("Object Position: (%f, %f)\n", objecPositionX_, objecPositionY_);
-              
+        // Set the X and Y coordinates
+        vec estimatedRobot_x_coordinates = {0.05};
+        vec estimatedRobot_y_coordinates = {0.0};
+    
+        // Create a 2xN matrix
+        mat points = join_vert(estimatedRobot_x_coordinates.t(), estimatedRobot_y_coordinates.t());
+    
+        // Create rotation matrix
+        double radians = datum::pi * robotRotationYaw / 180.0; // Convert degrees to radians
+        mat rotationMatrix = {
+            {cos(radians), sin(radians)},
+            {-sin(radians), cos(radians)}
+        };
+    
+        // Rotate points
+        mat rotatedPoints = rotationMatrix * points;
+    
+        // Use the coordinates after rotation
+        double newX1 = rotatedPoints(1, 0) + robotPositionX;
+        double newY1 = rotatedPoints(0, 0) + robotPositionY;
+    
+        // For other points, you can similarly use newX and newY values
+        // cout << "Rotated X Coordinates: " << newX1 << endl;
+        // cout << "Rotated Y Coordinates: " << newY1 << endl;
+        objecPositionX_ = newX1;
+        objecPositionY_ = newY1;
     }
+
     
 
     void robot_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) 
@@ -141,22 +145,25 @@ private:
         robotRotationZ_ = msg->pose.orientation.z;
         robotRotationW_ = msg->pose.orientation.w;
         robotRotationYaw_ = baha_calculation(robotRotationZ_, robotRotationW_);
-        rotate_coordinates(robotPositionX_, robotPositionY_,  polygon_latitude_, polygon_longitude_, robotRotationYaw_);
+        rotateCoordinates(robotPositionX_, robotPositionY_,  robotRotationYaw_);
         object_x.push_back(objecPositionX_);
         object_y.push_back(objecPositionY_);   
         
         if (object_x.size() > 3)
         {
-            RCLCPP_INFO(this->get_logger(), "Last 3 Robot Pose: (%f, %f), (%f, %f), (%f, %f)",
-            object_x[0], object_y[0], object_x[1], object_y[1], object_x[2], object_y[2]);
+            // RCLCPP_INFO(this->get_logger(), "Last 3 Robot Pose: (%f, %f), (%f, %f), (%f, %f)",
+            // object_x[0], object_y[0], object_x[1], object_y[1], object_x[2], object_y[2]);
             if (calculate == 1 && 0 == (std::any_of(lastDistance1.begin(), lastDistance1.end(), [](float distance) { return distance == 0; }) ||
                 std::any_of(lastDistance2.begin(), lastDistance2.end(), [](float distance) { return distance == 0; }) ||
                 std::any_of(lastDistance3.begin(), lastDistance3.end(), [](float distance) { return distance == 0; })))
             {
                 for (size_t i = 0; i < anchor_addr_list.size(); i++)
                 {
-                    RCLCPP_INFO(this->get_logger(), "Anchor %s - Distance: %f, RX Power: %f",
-                                anchor_addr_list[i].c_str(), distance_list[i], rx_power_list[i]);
+                    // RCLCPP_INFO(this->get_logger(), "Anchor %s - Distance: %f, RX Power: %f",
+                                // anchor_addr_list[i].c_str(), distance_list[i], rx_power_list[i]);
+                    std::cout << "robot pos" << msg->pose.position.x << " " << msg->pose.position.y << std::endl;
+                    std::cout << "object pos:" << object_x[0] << " " << object_y[0] << std::endl;
+                    std::cout << "robot angle " << robotRotationYaw_ << std::endl;
 
                     Point points[NUM_POINTS] = {{object_x[0], object_y[0]}, {object_x[1],object_y[1]}, {object_x[2],object_y[2]}};
                     double distances[NUM_POINTS] = {lastDistance1[i], lastDistance2[i], lastDistance3[i]};
@@ -185,7 +192,7 @@ private:
                         p = new_p;
                     }
 
-                    std::cout << "Estimated position: (" << p.x << ", " << p.y << ")" << std::endl;
+                    // std::cout << "Estimated position: (" << p.x << ", " << p.y << ")" << std::endl;
                 }
             }
             object_x.erase(object_x.begin());
